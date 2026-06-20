14b9bab6ce126f7c867e239b29da11bf76b522bd
Farzan Nedaei <fffarzan@Farzans-MacBook-Air.local>
2026-06-21 00:21:56 +0330

    add early fonts doc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,35 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { QueryProvider } from './common/providers/query-provider';
+import { Geist } from 'next/font/google';
+import localFont from 'next/font/local';
+
+// google font
+const geist = Geist({
+	weight: '400',
+	subsets: ['latin'],
+});
+
+// local font
+const roboto = localFont({
+	src: [
+		{
+			path: './fonts/Roboto-Regular.ttf',
+			weight: '400',
+			style: 'normal',
+		},
+		{
+			path: './fonts/Roboto-Light.ttf',
+			weight: '300',
+			style: 'normal',
+		},
+		{
+			path: './fonts/Roboto-Thin.ttf',
+			weight: '200',
+			style: 'normal',
+		},
+	],
+});
 
 export const metadata: Metadata = {
 	title: 'Nexter',
@@ -14,9 +43,9 @@ type RootLayoutPorps = Readonly<{
 // Root layout is required and must contain `html` and `body` tags.
 export default function RootLayout({ children }: RootLayoutPorps) {
 	return (
-		<html lang="en">
+		<html lang="en" className={geist.className}>
 			<QueryProvider>
-				<body>{children}</body>
+				<body className={roboto.className}>{children}</body>
 			</QueryProvider>
 		</html>
 	);
---
+++ docs/FONT.md
@@ -0,0 +1,22 @@
+# Font Module
+
+automatically optimizes your fonts and removes external network requests.
+
+built-in self-hosting for any font file. CSS and font files are downloaded at build time and self-hosted with the rest of your static assets. --> load web fonts with no layout shift.
+
+## Google Fonts
+
+automatically self-host. no requests are sent to Google by the browser.
+
+Note: If loading a variable font, you don't need to specify the font weight.
+
+# Refs
+
+- [fonts](https://nextjs.org/docs/app/getting-started/fonts)
+
+### continue
+
+- https://fonts.google.com/variablefonts
+- https://nextjs.org/docs/app/api-reference/components/font
+- https://web.dev/articles/cls
+- https://www.youtube.com/watch?v=L8_98i_bMMA
